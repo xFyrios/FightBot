457dc0333c57cd2441ea3475c4e76aaf638846eb
ZeDoCaixao <spam@vd.immoral.ru>
2019-02-05 12:21:30 +0300

    tabs…

--- fight/attack.py
+++ fight/attack.py
@@ -141,8 +141,8 @@ class Attack:
 	def is_weak_against(self, target_element):
 		"""Test if an attacks element is weak against target element"""
 		# Water > Fire > Earth > Wind > Water
-        if self.element_type == 0 or target_element == 0:
-            return False
+		if self.element_type == 0 or target_element == 0:
+			return False
 		if (self.element_type, target_element) in (
 				(ELEM_WATER, ELEM_WIND),
 				(ELEM_FIRE, ELEM_WATER),
